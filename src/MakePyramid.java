8892e2964244f4ab7708bae618c172be7463b5a1
Lobid Chowdhury <lobid.bd@gmail.com>
2018-12-16 16:00:46 -0500

    actual pyramid

--- src/MakePyramid.java
+++ src/MakePyramid.java
@@ -1,24 +1,23 @@
 public class MakePyramid {
     public static void main(String[] args) {
 
-        for (int i=0;i<20;i++){
 
-        for (int j=0;j<20-i;j++){
 
-            System.out.print(" ");
+        for (int i=0;i<10;i++) {
 
-            {
-         for (int k=0;k<=i; k++) {
-             System.out.print("* ");
+            for (int j = 0; j < 10 - i; j++) {
 
-         }
+                System.out.print(" ");
 
-                System.out.println();
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
 
+            }
+
+            System.out.println();
 
         }
-
         }
 
     }
-}}
